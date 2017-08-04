29e0cda57a3f8651571f5a15eae25b29e0a0c0b6
abhijeet-sonawane <abhijeet.sonawane@raweng.com>
2017-08-04 12:21:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = Built.App('blt917c46b3de8e419f')
 .setMasterKey('blte09616313f4eaf7a')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt917c46b3de8e419f',
+	secret_key     : 'bltc78359c97ace222b',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')

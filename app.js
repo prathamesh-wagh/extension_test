89d686aa92a26696ba93b8b60346a26d21ae5b97
abhijeet-sonawane <abhijeet.sonawane@raweng.com>
2017-08-04 12:38:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt917c46b3de8e419f')
+var app = Built.App('bltf738cc52904b76c6')
 // .setHost('localhost')
 // .setPort(8000)
-.setHost("dev-api.built.io")
-.setProtocol("https")
+.setHost("54c383fd.ngrok.io")
+.setProtocol("http")
 // .setPort(80)
 .setMasterKey('blte09616313f4eaf7a')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'bltc78359c97ace222b',
+	secret_key     : 'bltf738cc52904b76c6',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')

490fe1da825b87fc392aa234b2f32ead2b94d947
abhijeet-sonawane <abhijeet.sonawane@raweng.com>
2017-09-01 18:12:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App1('blt196d1ea350e6398e')
+var app = Built.App('blt196d1ea350e6398e')
 .setHost("fd238959.ngrok.io")
 .setProtocol("https")
 .setMasterKey('bltd99abe8292a3b1bb')

6e1043568617d3c61eb04aa64c48c7aca0621472
abhijeet-sonawane <abhijeet.sonawane@raweng.com>
2017-11-27 18:20:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltf3a6ab5171abc64d')
+var app = Built.App('blt2d31066205240aa3')
 .setHost("test-stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt13a96080e764d20b')
+.setMasterKey('bltcfadb1ee934a4681')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'blt2027aaf39aeb589c',

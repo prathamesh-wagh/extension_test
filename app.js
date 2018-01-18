e558373d123d934954056b3579574071af05e207
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-01-18 15:58:36 +0530

    Test Extension

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var app = Built.App('bltc4f7ed1f03053fe8')
 .setProtocol("https")
 .setMasterKey('bltc88f3d42ad8a728c')
 
-// e3ac0e0e1a8feb917676e4b750b7b072970304e7 - access token
-// bltc88f3d42ad8a728c - secret key
-// blt_ext_default_test - extension key
+// 55a98ecfbcfd5b50d5e2eba32568360e7b97c81b - access token
+// bltf5a0bc85b46a6472 - secret key
+// blt_ext_default_test_app - extension key
 // web hook url - https://test-stag-api.built.io/v1/extensions/blt_ext_default/redeploy?application_api_key=bltc4f7ed1f03053fe8
 
 var extensionSDK = app.Extension({
-	secret_key     : 'bltc88f3d42ad8a728c',
-	extension_key  : 'blt_ext_default_test',
+	secret_key     : 'blt1bfd0c51fb8bfa90',
+	extension_key  : 'blt_ext_default_test_app',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
 })
